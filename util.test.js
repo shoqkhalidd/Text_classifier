85bf041112c46083b7b989b035435f2eb36eefa1
shoqkhalidd <shoqkhalidd@gmail.com>
2021-12-08 00:05:23 +0300

    added a empty statement test case

--- util.test.js
+++ util.test.js
@@ -1,7 +1,14 @@
 const fast=require("./util.js")
 
-test("properly predicting",() => {
+test("Checking it can predict a statement",() => {
     const res= fast("Which baking dish is best to bake a banana bread ?");
     expect(res).toBe("success!")
+
+});
+
+
+test("Checking it fails when predicting an empty statement",() => {
+    const res= fast("");
+    expect(res).toBe("fail")
 
 });
